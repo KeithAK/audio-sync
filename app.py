cd70533ab1e760f0288f60fc324833be14eca79f
KeithAK <keith.knowles@gmx.com>
2024-10-28 14:49:24 +0100

    can now select video/audio files in the streamlit app. when selecting video files, an audio track must be selected as the reference/source track. Their sync offset values can then be calculated.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import streamlit as st
+from pathlib import Path
+from mkv_funcs import list_audio_tracks, extract_audio_track
 from audio_sync import find_offset
 
 FILE_DIR = 'tmp'
@@ -11,11 +13,25 @@ def file_selector(file_tag:str, folder_path=FILE_DIR):
 
 file_ref = file_selector(file_tag='reference')
 st.write('You selected `%s`' % file_ref)
+if Path(file_ref).suffix == '.mkv':
+    audio_tracks = list_audio_tracks(file_ref)
+    track_ids = [t[0] for t in audio_tracks]
+    st.write(audio_tracks)
+    track_id_sel_ref = int(st.radio('Select which audio track to use as reference', track_ids))-1
+    st.write(track_id_sel_ref)
 
 file_src = file_selector(file_tag='source')
 st.write('You selected `%s`' % file_src)
+if Path(file_src).suffix == '.mkv':
+    audio_tracks = list_audio_tracks(file_src)
+    track_ids = [t[0] for t in audio_tracks]
+    st.write(audio_tracks)
+    track_id_sel_src = int(st.radio('Select which audio track to use as source', track_ids))-1
+    st.write(track_id_sel_src)
 
 if st.button('Get offset'):
+    file_ref = extract_audio_track(file_ref, audio_tracks[track_id_sel_ref][0], audio_tracks[track_id_sel_ref][2])
+    st.write(file_ref)
     offsets, median, std_dev = find_offset(file_ref, file_src)
     if median:
         st.write(f'Offsets: {offsets}')
